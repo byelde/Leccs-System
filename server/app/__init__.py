aa03d47f965a12f5b4de2b1b43907315e16a5ded
gabryel-souza <byelacademico@gmail.com>
2024-10-23 19:25:24 -0300

    fix: not initializing app database

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -2,6 +2,17 @@ from flask import Flask
 from mvc_flask import FlaskMVC
 from flask_cors import CORS
 
+from app.database import db
+
+from app.models.Users.Teacher import Teacher
+from app.models.Users.Student import Student
+from app.models.Users.LeecsCoord import CoordLeccs
+
+from app.models.Events.Activity import Activity
+from app.models.Events.Requisition import Requisition
+
+
+
 def create_app(SQLITE_URI: str, SECRET_KEY: str) -> Flask:
 
     app: type[Flask] = Flask(__name__)
@@ -19,3 +30,8 @@ app = create_app(
         SQLITE_URI= "sqlite:///mydatabase.db",
         SECRET_KEY="SECRET_KEY"
     )
+
+
+with app.app_context():
+    db.init_app(app)
+    db.create_all()
