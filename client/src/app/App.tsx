e954dc8b139ab5239fdc2e18a7b00cf2796bf482
gabryel-souza <byelacademico@gmail.com>
2024-11-01 20:09:58 -0300

    ✨ feat: routes

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,12 +1,10 @@
+import { Routes } from "./routes"
+import "./App.css"
 
 function App() {
 
   return (
-    <>
-      <div>
-        <h1>Hello</h1>
-      </div>
-    </>
+    <Routes/>
   )
 }
 
--- client/src/app/routes/index.tsx
+++ client/src/app/routes/index.tsx
@@ -1,1 +1,13 @@
-export { }
+import { BrowserRouter, Navigate, Route, Routes as Switch } from "react-router-dom"
+import { Login } from "../pages"
+
+export const Routes = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="*" element={ <Navigate to={"/login"}/> }/>
+            </Switch>
+        </BrowserRouter>
+    )
+}
