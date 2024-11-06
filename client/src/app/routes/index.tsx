c9e012e0c72e1c7707798f3ef2a3bd678a83fae6
gabryel-souza <byelacademico@gmail.com>
2024-11-06 12:02:32 -0300

    ✨ feat: routering homepage

--- client/src/app/routes/index.tsx
+++ client/src/app/routes/index.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Navigate, Route, Routes as Switch } from "react-router-dom"
-import { Login } from "../pages"
+import { Homepage, Login } from "../pages"
 
 export const Routes = () => {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/login" element={<Login/>}/>
+                <Route path="/" element={<Homepage/>}/>
                 <Route path="*" element={ <Navigate to={"/login"}/> }/>
             </Switch>
         </BrowserRouter>
