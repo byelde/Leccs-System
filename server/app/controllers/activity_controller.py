277c0675d48afc1cdf5a148b051e233009d02501
gabryel-souza <byelacademico@gmail.com>
2024-11-19 08:16:43 -0300

    ✨ feat: "POOing" the activitiy_controller

--- server/app/controllers/activity_controller.py
+++ server/app/controllers/activity_controller.py
@@ -4,13 +4,13 @@ from datetime import datetime
 
 from app.database import db
 
+from app.models.Users.IUser import User
 from app.models.Events.Activity import Activity
 from app.models.Events.Requisition import Requisition
 
 class ActivityController:
     
     def create(self):
-
         data = request.json
         
         responsible_id: str = data.get("responsible_id")
@@ -22,8 +22,19 @@ class ActivityController:
 
 
         if not (responsible_id and category and lecc_id and init_date and end_date):
-            return( jsonify({"message":"data is missing."}), 400 )
+            return(jsonify({"message":"data is missing."}), 400)
         
+        from app.models.Users.LeecsCoord import CoordLeccs
+        from app.models.Users.Teacher import Teacher
+        from app.models.Users.Student import Student
+
+        user_obj:type[User]
+
+        user_obj = CoordLeccs.query.filter_by(_teacher_id = responsible_id).first()
+        if not (user_obj): user_obj = Teacher.query.filter_by(_id = responsible_id).first()
+        if not (user_obj): user_obj = Student.query.filter_by(_id = responsible_id).first()
+
+        print(user_obj)
 
         try:
             act_obj: type[Activity] = Activity(
@@ -34,23 +45,13 @@ class ActivityController:
                 lecc_id = lecc_id,
                 description = description
             )
-            act_obj.sendActivity()
+            user_obj.bookLecc(act_obj)
 
         except Exception as e:
-            return ( jsonify({"message":str(e)}), 400 )
+            return (jsonify({"message":str(e)}), 400)
+            
 
-
-        try:
-            req_obj: type[Requisition] = Requisition(act_obj.getId())
-            req_obj.sendRequisition()
-
-        except Exception as e:
-            self.delete(act_obj.getId())
-            return ( jsonify({"message":str(e)}), 400 )
-
-
-
-        return ( jsonify({"message":"request send successfully."}), 200 )
+        return (jsonify({"message":"request send successfully."}), 200)
     
     
     def show(self, id):
@@ -58,29 +59,15 @@ class ActivityController:
         act_id = request.json.get("id")
 
         if not act_id:
-            return(
-                jsonify({
-                    "message":"data is missing."
-                }),
-                400
-            )
+            return(jsonify({"message":"data is missing."}), 400)
 
         try:
             act_obj: type[Activity] = Activity.query.filter_by(_id=act_id).first()
 
         except Exception as e:
-            return(
-                jsonify({
-                    "message":str(e)
-                }),
-                400
-            )
+            return(jsonify({"message":str(e)}), 400)
         
-
-        return jsonify(
-            act_obj.to_json(),
-            200
-        )
+        return jsonify(act_obj.to_json(), 200)
     
 
     def showAll(self, query):
@@ -95,7 +82,6 @@ class ActivityController:
         min_date:   datetime = datetime.fromisoformat(args.get("min_date")) if args.get("min_date") else datetime.min
         state:      bool = args.get("state") if (args.get("state") != None) else None
 
-        print(id,category,lecc_id,max_date,min_date)
 
         act_obj_list: list[Activity] = db.session.query(Activity).filter(
             ((Activity._id == id) if (id) else (Activity._id != id)),
@@ -105,6 +91,7 @@ class ActivityController:
             Activity._init_date.between(min_date, max_date),
             ((Activity._state == state) if (state != None) else (True))
         ).all()
+
 
         return (jsonify([
             obj.to_json() for obj in act_obj_list
@@ -123,84 +110,78 @@ class ActivityController:
         lecc_id    : str = args.get("lecc_id")
         description: str = args.get("description")
 
+        coord_id: str = args.get("coord_id")
+
+        if (coord_id):
+            from app.models.Users.LeecsCoord import CoordLeccs
+
+            coord_obj:type[CoordLeccs] = CoordLeccs.query.filter_by(_teacher_id = coord_id).first()
+
+            if not(coord_obj):
+                return(jsonify({"message": "permission denied."}), 400)
+            
+            try:
+                coord_obj.acceptRequisition(id)
+
+                return(jsonify({"message": "requisition accepted."}), 200)
+            
+            except Exception as e:
+
+                return (jsonify({"message":str(e)}), 400)
+
 
         if not (id and (category or init_date or end_date or lecc_id or description)):
-            return (
-                jsonify({
-                    "message":"data is missing."
-                }),
-                400
-            )
+            return (jsonify({"message":"data is missing."}), 400)
         
         act_obj: type[Activity] = Activity.query.filter_by(_id = id).first()
 
         if act_obj is None:
-            return (
-                jsonify({
-                    "message":"activity not found."
-                }),
-                400
-            )
+            return (jsonify({"message":"activity not found."}), 400)
 
         act_obj.updateData(
             category,
             init_date,
             end_date,
             lecc_id,
-            description
+            description,
         )
 
         try:
             db.session.commit()
 
         except Exception as e:
-            return (
-                jsonify({
-                    "message":str(e)
-                }),
-                400
-            )
+            return (jsonify({"message":str(e)}), 400)
         
 
-        return(
-            jsonify({
-                "message": "activity updated."
-            }),
-            200
-        )
+        return(jsonify({"message": "activity updated."}), 200)
 
              
     def delete(self, id):
 
+        data = request.args
+        id = data.get("id")
+        resp_id = data.get("resp_id")
+
+        from app.models.Users.LeecsCoord import CoordLeccs
+        from app.models.Users.Teacher import Teacher
+        from app.models.Users.Student import Student
+
+        user_obj:type[User]
+
+        user_obj = CoordLeccs.query.filter_by(_teacher_id = resp_id).first()
+        if not (user_obj): user_obj = Teacher.query.filter_by(_id = resp_id).first()
+        if not (user_obj): user_obj = Student.query.filter_by(_id = resp_id).first()
+
         try:
             id = int(id)
         except:
-            return (
-                jsonify({
-                    "message":"invalid activity id."
-                }),
-                400
-            )
+            return (jsonify({"message":"invalid activity id."}), 400)
         
-
         try:
-            Requisition.query.filter_by(_activity_id = id).delete()
-            Activity.query.filter_by(_id = id).delete()
-            db.session.commit()
-            db.session.flush()
+            act_obj:type[Activity] = Activity.query.filter_by(_id = id).first()
+            user_obj.cancelBook(act_obj)
+            return (jsonify({"message":"activity deleted."}), 200)
 
         except Exception as e:
-            return (
-                jsonify({
-                    "message":str(e)
-                }),
-                400
-            )
+            return (jsonify({"message":str(e)}), 400)
 
-
-        return (
-            jsonify(
-                {"message":"activity deleted."}
-            ),
-            200
-        )
